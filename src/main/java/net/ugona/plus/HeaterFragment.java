bc1de653a88eba30a4d2c7b1fedde2f6992f02da
Vladimir Shutoff <vladimir@shutoff.ru>
2014-05-28 20:20:41 +0400

    Heater timer

--- src/main/java/net/ugona/plus/HeaterFragment.java
+++ src/main/java/net/ugona/plus/HeaterFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Vector;
+
 public class HeaterFragment extends DeviceFragment {
 
     @Override
@@ -21,6 +23,7 @@ public class HeaterFragment extends DeviceFragment {
     }
 
     void fill() {
+        items = new Vector<Item>();
         items.add(new SettingsFragment.ListItem(R.string.connection, R.array.heater_values, R.array.heater, Names.Car.CAR_RELE, "1"));
         items.add(new CheckBoxItem(R.string.impulse, Names.Car.RELE_IMPULSE, true));
         items.add(new SeekBarItem(R.string.heater_time, 13, 0, 255, R.string.minutes));
@@ -28,6 +31,22 @@ public class HeaterFragment extends DeviceFragment {
 
         TimerCommands cmd = new TimerCommands();
         String rele = preferences.getString(Names.Car.CAR_RELE + car_id, "1");
+        if (rele.equals("1")) {
+            if (preferences.getBoolean(Names.Car.RELE_IMPULSE + car_id, true)) {
+                cmd.add(new TimerCommand(3, 0, R.string.heater_on));
+            } else {
+                cmd.add(new TimerCommand(5, R.drawable.icon_heater, R.string.heater_on));
+                cmd.add(new TimerCommand(4, R.drawable.icon_heater_on, R.string.heater_off));
+            }
+        }
+        if (rele.equals("2")) {
+            if (preferences.getBoolean(Names.Car.RELE_IMPULSE + car_id, true)) {
+                cmd.add(new TimerCommand(6, 0, R.string.heater_on));
+            } else {
+                cmd.add(new TimerCommand(8, R.drawable.icon_heater, R.string.heater_on));
+                cmd.add(new TimerCommand(7, R.drawable.icon_heater_on, R.string.heater_off));
+            }
+        }
         if (rele.equals("3")) {
             cmd.add(new TimerCommand(3, R.drawable.icon_heater, R.string.heater_on));
             cmd.add(new TimerCommand(6, R.drawable.icon_heater_air, R.string.heater_air));
