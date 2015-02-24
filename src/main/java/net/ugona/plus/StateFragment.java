e42da516ce6e23813ba435878be03d020748d266
Vladimir Shutoff <vovan@shutoff.ru>
2015-02-25 00:15:27 +0300

    State view stub

--- src/main/java/net/ugona/plus/StateFragment.java
+++ src/main/java/net/ugona/plus/StateFragment.java
@@ -1,4 +1,16 @@
 package net.ugona.plus;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 public class StateFragment extends MainFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.state, container, false);
+        return v;
+    }
 }
